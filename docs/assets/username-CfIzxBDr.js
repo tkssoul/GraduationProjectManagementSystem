483667d917ae3deab3558b7fa07feadd11ad452d
tkssoul <2495830486@qq.com>
2025-06-03 18:23:55 +0800

    build:发布v1.0.0版本

---
+++ docs/assets/username-CfIzxBDr.js
@@ -0,0 +1,1 @@
+import{d as e}from"./pinia-DSM-4MLd.js";import{h as r}from"./vendor-ChWzW76f.js";const s=e("username",(()=>{const e=r("");return{username:e,setUsername:function(r){e.value=r}}}));export{s as u};
